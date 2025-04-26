0a72d047a6e65849b51c804108b972d9bb06b366
Russell Semsem <109185074+russellsemsem@users.noreply.github.com>
2025-04-26 01:58:39 -0700

    added upload pdf service

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+from app.services.file_upload_service import save_uploaded_pdf
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello --CareFlow Server"}
 
-
 @app.get("/health")
 async def health():
-    return {"status": "healthy"}  
+    return {"status": "healthy"}
+
+@app.post("/upload-pdf")
+async def upload_pdf(file: UploadFile = File(...)):
+    try:
+        filename = await save_uploaded_pdf(file)
+        return JSONResponse(content={"filename": filename, "message": "Upload successful!"})
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error.")
