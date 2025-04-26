53c15d0e0ff78f2172e2feda66f4d22b0dc0edd8
Russell Semsem <109185074+russellsemsem@users.noreply.github.com>
2025-04-26 01:22:37 -0700

    init commit, server

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello --CareFlow Server"}
+
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy"}  
